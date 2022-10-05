4192dbf2e6539b93617aa2fdbcc65c3c8daaa7b9
Mengzhen Liao <liaomengzhen@gmail.com>
2022-10-05 21:06:08 +0200

    add team Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Home content here</h1>
-      <a>This is our team</a>
+      <a>
+        This is our <Link href="/team">team</Link>
+      </a>
     </>
   );
 }
